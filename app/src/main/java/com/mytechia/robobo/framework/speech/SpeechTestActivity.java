f246849b2462826bd8ebb31450c7c25df22daad9
Luis Llamas <lfllamas93@gmail.com>
2016-10-25 09:49:56 +0200

    Initial commit

---
+++ app/src/main/java/com/mytechia/robobo/framework/speech/SpeechTestActivity.java
@@ -0,0 +1,13 @@
+package com.mytechia.robobo.framework.speech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_test);
+    }
+}

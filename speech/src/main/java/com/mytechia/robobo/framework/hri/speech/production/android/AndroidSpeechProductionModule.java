0139b8c377f710a2ad29a8de87003bd49fd695e3
Shiul93 <lfllamas93@gmail.com>
2016-12-21 10:34:02 +0100

    Updated javadoc, headers and dependencies

--- speech/src/main/java/com/mytechia/robobo/framework/hri/speech/production/android/AndroidSpeechProductionModule.java
+++ speech/src/main/java/com/mytechia/robobo/framework/hri/speech/production/android/AndroidSpeechProductionModule.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ *
+ *   Copyright 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ *   Copyright 2016 Luis Llamas <luis.llamas@mytechia.com>
+ *
+ *   This file is part of Robobo HRI Modules.
+ *
+ *   Robobo HRI Modules is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Robobo HRI Modules is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Robobo HRI Modules.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
 package com.mytechia.robobo.framework.hri.speech.production.android;
 
 
@@ -15,10 +36,13 @@ import com.mytechia.robobo.framework.RoboboManager;
 import com.mytechia.robobo.framework.hri.speech.production.ISpeechProductionModule;
 import com.mytechia.robobo.framework.hri.speech.production.ITtsVoice;
 import com.mytechia.robobo.framework.hri.speech.production.VoiceNotFoundException;
+import com.mytechia.robobo.framework.remote_control.remotemodule.Command;
+import com.mytechia.robobo.framework.remote_control.remotemodule.ICommandExecutor;
+import com.mytechia.robobo.framework.remote_control.remotemodule.IRemoteControlModule;
 
 
 /**
- * Created by luis on 5/4/16.
+ * Implementation of the speech production module
  */
 public class AndroidSpeechProductionModule implements ISpeechProductionModule {
 
@@ -27,6 +51,7 @@ public class AndroidSpeechProductionModule implements ISpeechProductionModule {
     private Locale loc = null;
     private Context context = null;
     Collection<ITtsVoice> voices = null;
+    private String TAG = "AnsdroidSpeechP";
     //endregion
 
     //region ISpeechProductionModule Methods
@@ -171,12 +196,17 @@ public class AndroidSpeechProductionModule implements ISpeechProductionModule {
 
 
 
+        roboboManager.getModuleInstance(IRemoteControlModule.class).registerCommand("TALK", new ICommandExecutor() {
+            @Override
+            public void executeCommand(Command c, IRemoteControlModule rcmodule) {
+                sayText(c.getParameters().get("text"),PRIORITY_HIGH);
+            }
+        });
 
 
 
 
     }
-
 
 
     @Override
@@ -201,6 +231,8 @@ public class AndroidSpeechProductionModule implements ISpeechProductionModule {
     public String getModuleVersion() {
         return "0.1";
     }
+
+
 
     //endregion
 
--- speech/src/main/java/com/mytechia/robobo/framework/hri/speech/recognition/pocketsphinx/PocketSphinxSpeechRecognitionModule.java
+++ speech/src/main/java/com/mytechia/robobo/framework/hri/speech/recognition/pocketsphinx/PocketSphinxSpeechRecognitionModule.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ *
+ *   Copyright 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ *   Copyright 2016 Luis Llamas <luis.llamas@mytechia.com>
+ *
+ *   This file is part of Robobo HRI Modules.
+ *
+ *   Robobo HRI Modules is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Robobo HRI Modules is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Robobo HRI Modules.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
 package com.mytechia.robobo.framework.hri.speech.recognition.pocketsphinx;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -30,7 +52,7 @@ import static edu.cmu.pocketsphinx.SpeechRecognizerSetup.defaultSetup;
 
 
 /**
- * Created by luis on 5/4/16.
+ * Implementation of the Robobo speech recognition module usng the PocketSphinx library
  */
 public class PocketSphinxSpeechRecognitionModule extends ASpeechRecognitionModule implements RecognitionListener {
 

70d377cd3d5f1cd5dc90e93d3d3b11fe118577c9
Shiul93 <lfllamas93@gmail.com>
2018-11-19 13:02:13 +0100

    Updated comments

--- speech/src/main/java/com/mytechia/robobo/framework/hri/speech/production/ISpeechProductionListener.java
+++ speech/src/main/java/com/mytechia/robobo/framework/hri/speech/production/ISpeechProductionListener.java
@@ -4,6 +4,9 @@ package com.mytechia.robobo.framework.hri.speech.production;
  * Created by luis on 28/6/17.
  */
 
+/**
+ * Listener for notifications on speech production events
+ */
 public interface ISpeechProductionListener {
     /**
      * Notifies when a phrase ends

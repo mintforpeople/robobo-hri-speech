02e377b2b80b08e68a2c09753c195a95ad614e7f
rodrigo-mintin <rodrigo.martin@mintforpeople.com>
2023-11-21 10:05:28 +0100

    Updated spanish and english models to recognize new words (such as Robobo 👀)

--- app/src/main/java/com/mytechia/robobo/framework/speech/SpeechTestActivity.java
+++ app/src/main/java/com/mytechia/robobo/framework/speech/SpeechTestActivity.java
@@ -3,24 +3,36 @@ package com.mytechia.robobo.framework.speech;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.mytechia.robobo.framework.RoboboManager;
 import com.mytechia.robobo.framework.exception.ModuleNotFoundException;
 import com.mytechia.robobo.framework.hri.speech.production.ISpeechProductionModule;
 import com.mytechia.robobo.framework.hri.speech.recognition.ISpeechDetectionModule;
+import com.mytechia.robobo.framework.hri.speech.recognition.ISpeechListener;
 import com.mytechia.robobo.framework.service.RoboboServiceHelper;
+
+
 
 public class SpeechTestActivity extends AppCompatActivity {
 
     private ISpeechProductionModule productionModule;
     private ISpeechDetectionModule detectionModule;
     private RoboboManager manager;
+    private TextView SpeechTextView;
 
+    private class SpeechListener implements ISpeechListener{
+        @Override
+        public void onResult(String s) {
+            SpeechTextView.append(s + "\n");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech_test);
+        SpeechTextView = findViewById(R.id.speechTextView);
         RoboboServiceHelper serviceHelper = new RoboboServiceHelper(this, new RoboboServiceHelper.Listener() {
             @Override
             public void onRoboboManagerStarted(RoboboManager roboboManager) {
@@ -38,11 +50,15 @@ public class SpeechTestActivity extends AppCompatActivity {
 
     }
 
-
     public void startapp(){
         try {
             productionModule = manager.getModuleInstance(ISpeechProductionModule.class);
             detectionModule = manager.getModuleInstance(ISpeechDetectionModule.class);
+
+            SpeechListener listener = new SpeechListener();
+            detectionModule.suscribeAny(listener);
+            detectionModule.toggleDetection(true);
+
         } catch (ModuleNotFoundException e) {
             e.printStackTrace();
         }
